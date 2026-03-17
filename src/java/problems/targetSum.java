01a3ac9b51489f0e08a01ab903fdc186ac3981c2
Fortune Nkambule <FortuneT711@outlook.com>
2026-03-17 19:43:55 +0200

    target-sum Added

---
+++ src/java/problems/targetSum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> hash = new HashMap<>();
+
+        for(int i = 0 ; i < nums.length; i++){
+
+            int res = target - nums[i];
+
+            if(hash.containsKey(res)){
+                return new int[]{i,hash.get(res)};
+            }
+            else{
+                hash.put(nums[i], i);
+            }
+
+
+        }
+        return new int[]{};
+    }
+}
